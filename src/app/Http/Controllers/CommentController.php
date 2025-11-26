c95186a26079e3a58b8cf9da6218514a0eb7d349
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2025-11-26 22:36:52 +0900

    環境構築

---
+++ src/app/Http/Controllers/CommentController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // 全データ取得
+        $items = Comment::all();
+        return response()->json([
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // データ追加
+        $item = Comment::create($request->all());
+        return response()->json([
+            'data' => $item
+        ], 201);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comment)
+    {
+        // データ削除
+        $item = Comment::where('id', $comment->id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => 'Deleted successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+}
